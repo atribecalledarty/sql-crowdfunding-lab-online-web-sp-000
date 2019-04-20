1b641b4d06d521aac448f9d943cb358f33fd416c
Arthur Song <arth3rs0ng@gmail.com>
2019-04-20 02:00:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,14 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
 ("Pay for FlatIron", "Tuition", 7500, "2019-03-23", "2020-03-23"),
+("Pay for my Rent", "Living Expenses", 1000, "2019-03-23", "2020-03-23"),
+("Pay for my Gym Membership", "Fitness", 50, "2019-03-23", "2020-03-23"),
+("Pay for my Food Expenses", "Living Expenses", 300, "2019-03-23", "2020-03-23"),
+("Pay for my Piano Lessons", "Hobby", 7500, "2019-03-23", "2020-03-23"),
+("Pay for my Gas expenses", "Living Expenses", 200, "2019-03-23", "2020-03-23"),
+("Pay for my Clothes", "Donation", 300, "2019-03-23", "2020-03-23"),
+("Pay for my Mom's new home", "Donation", 300000, "2019-03-23", "2020-03-23"),
+("Pay for my dream car", "Donation", 50000, "2019-03-23", "2020-03-23"),
+("Humane Society", "Charity", 1000, "2019-03-23", "2020-03-23");
+
+INSERT INTO user (name, age) VALUES
+("Bob", 23),
