5abccbcbe7ec25a967be71c7835340a45feb9e60
Arthur Song <arth3rs0ng@gmail.com>
2019-04-20 01:56:54 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,2 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
-("Pay for FlatIron", "Tuition", 7500, "")
+("Pay for FlatIron", "Tuition", 7500, "2019-03-23", "2020-03-23"),
